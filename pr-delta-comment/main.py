1e5e7cb52439e2268bc28fda4d320c754beb4ac9
Owen Pickford <owen.pickford@snyk.io>
2024-08-20 15:36:21 -0700

    Add bash handling of other product tests

--- pr-delta-comment/main.py
+++ pr-delta-comment/main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from modules import app_setup
 from modules import snyk_scan
 from modules import baseline
@@ -11,23 +12,36 @@ logging.basicConfig(
 )
 
 def main():
+    # Confirm environment is setup correctly
     app_setup.check_token()
     app_setup.check_org_id()
     app_setup.check_org_slug()
     ci_tool = app_setup.check_ci_tool()
-    
+
     # Authenticate Snyk CLI
     snyk_scan.auth()
+    
+    # Run the specified CI delta tests
+    if len(sys.argv) > 2:
+        print("You must pass one option: <test>, <code test>, <os test>, or <iac test>.")
+    elif sys.argv == "test":
+        print("Let's check everything!")
+    elif sys.argv == "code test":
+        print("Let's do a code test!")
+    elif sys.argv == "iac test":
+        print("Let's do an iac test!")
+    elif sys.argv == "os test":
+        # Get the baseline by either running monitor or using config 
+        delta = snyk_scan.delta(baseline.get_project_id(ci_tool))
 
-    # Get the baseline by either running monitor or using config 
-    delta = snyk_scan.delta(baseline.get_project_id(ci_tool))
+        # Put the delta data into JSON
+        formatted_delta = format.extract_vulns(delta)
 
-    # Put the delta data into JSON
-    formatted_delta = format.extract_vulns(delta)
+        markdown = format.create_markdown(formatted_delta)
 
-    markdown = format.create_markdown(formatted_delta)
-
-    pr_comment_api.add_comment(ci_tool, markdown)
+        pr_comment_api.add_comment(ci_tool, markdown)
+    else:
+        print("You must pass one option: <test>, <code test>, <os test>, or <iac test>.")
 
 if __name__ == "__main__":
     main()
